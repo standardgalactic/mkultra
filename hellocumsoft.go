092ca42d6aeeb99f98b763ca7d0b54dd0dcd7e11
cumsoft <97250816+cumsoft@users.noreply.github.com>
2022-02-26 00:24:06 -0500

    Update hellocumsoft.go

--- hellocumsoft.go
+++ hellocumsoft.go
@@ -8,3 +8,19 @@ func main() {
 	// Print “Hello, World!” to console
 	fmt.Println("Hello, Cumsoft!")
 }
+// Please Remove Section Below to run Program. 
+// Please Reference https://pkg.go.dev/net/http#Transport & https://www.socketloop.com/references/golang-net-http-transport-type-examples
+
+
+  // Method One
+  t := &http.Transport{}
+  t.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
+  c := &http.Client{Transport: t}
+  res, err := c.Get("file:///etc/passwd")
+
+// Method Two
+var httpClient = &http.Client{Transport: &transport{
+     t: http.Transport{
+         Dial: timeoutDial,
+         ResponseHeaderTimeout: *requestTimeout / 2,
+     }}}
