f6c3854d9f0c363ff258fb159fcfe2789b5b5bcb
C̵̢̢̙̲̦̝̠̪̻͎̥̦͙̒͜ů̵̢̜̟̮̩̙̟̝̳̠̈̏͌̑̄̎̇̈́̌͜ͅm̸͙̩̜̞͍̱̘̲̱̗͚̹̝̦̞̖̋̍̊̅͆̉̑͋́͐̎̈̇̑͆͝s̴̺̘͈̩̜̥̼̗͗͂̿̀́̑̏̄̈̒̋ô̴̫̻͜f̸̡͎̼̣̞͆͌̂̄͐̑̂͘̚t̴̥̟͉̆̾̉͑̂̓̅́̚͘͝ <97250816+cumsoft@users.noreply.github.com>
2022-06-06 01:31:36 -0400

    Create cumsoftonclick.java

---
+++ cumsoftonclick.java
@@ -0,0 +1,12 @@
+package org.jsweet;
+
+import static def.dom.Globals.*;
+
+/**
+ * This is a very simple example that just shows an alert.
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		alert("Cumsoft!");
+	}
+} 
