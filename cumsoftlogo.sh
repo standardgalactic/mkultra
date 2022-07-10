3b89ebdc0848be6491ed642698d3ebb5a0095167
c̥ͦu̥ͦm̥ͦs̥ͦo̥ͦf̥ͦt̥ͦ <97250816+cumsoft@users.noreply.github.com>
2022-07-10 13:53:50 -0400

    Create cumsoftlogo.sh

---
+++ cumsoftlogo.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+#
+#WWWWWWWWWWWWWWWWWWWWWWWWNXK000000KXNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+#WWWWWWWWWWWWWWWWWNKkdl:,'...........,;cok0NWWWWWWWWWWWWWWWWWWWWWWWW
+#WWWWWWWWWWWWWWKxl,.        .;.           .'cd0NWWWWWWWWWWWWWWWWWWWW
+#WWWWWWWWWWWXx:.            oW0xl.            .;o0WWWWWWWWWWWWWWWWWW
+#WWWWWWWWW0l.              .kMMMWO.              .:kNWWWWWWWWWWWWWWW
+#WWWWWWW0c.                '0MMMMWd                 ;ONWWWWWWWWWWWWW
+#WWWWWXd.                  .xWMMMWo                  .cKWWWWWWWWWWWW
+#WWWW0;               ..    .ckKXx.      .;.           'kWWWWWWWWWWW
+#WWWO'               .dO:.    .lkc    .,o0X:            .xNWWWWWWWWW
+#WWO'                '0MW0l.  ,k0c .:xKWMMWc             .dWWWWWWWWW
+#WK,                 .kMMMWO' lNK;.kWMMMMMK,              .kWWWWWWWW
+#Nl                   'OWMMN:.OMk.oMMMMMMXc                ;KWWWWWWW
+#0'                    .;ldc.lXXc lWMMWKd,                 .xWWWWWWW
+#d                          ,00l, .,:;,.                    :WWWWWWW
+#c                        .c0NKOkoclc.                      ,WWWWWWW
+#:                     .;lcdXO,'cd;'cd:                     'WWWWWWW
+#c                   .cl:. lNl   'oc..oo'                   'WWWWWWW
+#o                .;lc,    oK,    .:o,.;dc.                 ;WWWWWWW
+#O.              .ox.      cx.      'ol..ld,                oWWWWWWW
+#N:              ;0k'      ':.       .dd. ,dl.             ,0WWWWWWW
+#WO.             od;lc.             .:o;   .cd,           .dWWWWWWWW
+#WWx.            :x;.:o,          ,cl;.      lk.          lNWWWWWWWW
+#WWNd.            'do.'ll.    .,:lc'       .:d:          cXWWWWWWWWW
+#WWWWk.            .cx:.;oc,,:cl;.       'cl:.         .oNWWWWWWWWWW
+#WWWWW0:             'dd'.;:'.        .:lc,           ,kWWWWWWWWWWWW
+#WWWWWWNx,             :xc.         'cl:.           .oXWWWWWWWWWWWWW
+#WWWWWWWWXx,            .ox:.    .:lc'            'oKWWWWWWWWWWWWWWW
+#WWWWWWWWWWNkc.           ,xxlclll:.           .:xXWWWWWWWWWWWWWWWWW
+#WWWWWWWWWWWWWXkl,.         ';;,.          .'cxKNWWWWWWWWWWWWWWWWWWW
+#WWWWWWWWWWWWWWWWNKko:,..            ..,:lx0NWWWWWWWWWWWWWWWWWWWWWWW
+#WWWWWWWWWWWWWWWWWWWWWNKOxolc:::::coxOKNWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+#WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+#
+#
+# ▄▀▄▄▄▄   ▄▀▀▄ ▄▀▀▄  ▄▀▀▄ ▄▀▄  ▄▀▀▀▀▄  ▄▀▀▀▀▄   ▄▀▀▀█▄    ▄▀▀▀█▀▀▄ 
+# █ █    ▌ █   █    █ █  █ ▀  █ █ █   ▐ █      █ █  ▄▀  ▀▄ █    █  ▐ 
+# ▐ █      ▐  █    █  ▐  █    █    ▀▄   █      █ ▐ █▄▄▄▄   ▐   █     
+#   █        █    █     █    █  ▀▄   █  ▀▄    ▄▀  █    ▐      █      
+#  ▄▀▄▄▄▄▀    ▀▄▄▄▄▀  ▄▀   ▄▀    █▀▀▀     ▀▀▀▀    █         ▄▀       
+# █     ▐             █    █     ▐               █         █         
+# ▐                   ▐    ▐                     ▐         ▐         
+#
+#    Copyright © 2022-Present by Cumsoft. All rights reserved.
+#            This page was last updated on 09 july 2022
+#
+#                https://cumsoft.wixsite.com/cumsoft
+#                   https://github.com/cumsoft
+#                   https://cumsoft.gumroad.com 
+#
+Start
+sleep 2
+echo "WWWWWWWWWWWWWWWWWWWWWWWWNXK000000KXNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+echo "WWWWWWWWWWWWWWWWWNKkdl:,'...........,;cok0NWWWWWWWWWWWWWWWWWWWWWWWW"
+echo "WWWWWWWWWWWWWWKxl,.        .;.           .'cd0NWWWWWWWWWWWWWWWWWWWW"
+echo "WWWWWWWWWWWXx:.            oW0xl.            .;o0WWWWWWWWWWWWWWWWWW"
+echo "WWWWWWWWW0l.              .kMMMWO.              .:kNWWWWWWWWWWWWWWW"
+echo "WWWWWWW0c.                '0MMMMWd                 ;ONWWWWWWWWWWWWW"
+echo "WWWWWXd.                  .xWMMMWo                  .cKWWWWWWWWWWWW"
+echo "WWWW0;               ..    .ckKXx.      .;.           'kWWWWWWWWWWW"
+echo "WWWO'               .dO:.    .lkc    .,o0X:            .xNWWWWWWWWW"
+echo "WWO'                '0MW0l.  ,k0c .:xKWMMWc             .dWWWWWWWWW"
+echo "WK,                 .kMMMWO' lNK;.kWMMMMMK,              .kWWWWWWWW"
+echo "Nl                   'OWMMN:.OMk.oMMMMMMXc                ;KWWWWWWW"
+echo "0'                    .;ldc.lXXc lWMMWKd,                 .xWWWWWWW"
+echo "d                          ,00l, .,:;,.                    :WWWWWWW"
+echo "c                        .c0NKOkoclc.                      ,WWWWWWW"
+echo ":                     .;lcdXO,'cd;'cd:                     'WWWWWWW"
+echo "c                   .cl:. lNl   'oc..oo'                   'WWWWWWW"
+echo "o                .;lc,    oK,    .:o,.;dc.                 ;WWWWWWW"
+echo "O.              .ox.      cx.      'ol..ld,                oWWWWWWW"
+echo "N:              ;0k'      ':.       .dd. ,dl.             ,0WWWWWWW"
+echo "WO.             od;lc.             .:o;   .cd,           .dWWWWWWWW"
+echo "WWx.            :x;.:o,          ,cl;.      lk.          lNWWWWWWWW"
+echo "WWNd.            'do.'ll.    .,:lc'       .:d:          cXWWWWWWWWW"
+echo "WWWWk.            .cx:.;oc,,:cl;.       'cl:.         .oNWWWWWWWWWW"
+echo "WWWWW0:             'dd'.;:'.        .:lc,           ,kWWWWWWWWWWWW"
+echo "WWWWWWNx,             :xc.         'cl:.           .oXWWWWWWWWWWWWW"
+echo "WWWWWWWWXx,            .ox:.    .:lc'            'oKWWWWWWWWWWWWWWW"
+echo "WWWWWWWWWWNkc.           ,xxlclll:.           .:xXWWWWWWWWWWWWWWWWW"
+echo "WWWWWWWWWWWWWXkl,.         ';;,.          .'cxKNWWWWWWWWWWWWWWWWWWW"
+echo "WWWWWWWWWWWWWWWWNKko:,..            ..,:lx0NWWWWWWWWWWWWWWWWWWWWWWW"
+echo "WWWWWWWWWWWWWWWWWWWWWNKOxolc:::::coxOKNWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+echo "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+echo "cumsoft"
