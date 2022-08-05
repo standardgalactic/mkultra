2a43c30ddf29f8ee689019cb076b83161450b2ae
Cumsoft <97250816+cumsoft@users.noreply.github.com>
2022-08-05 00:22:01 -0400

    Create Cumsoftlogo.ps1

---
+++ Cumsoftlogo.ps1
@@ -0,0 +1,85 @@
+<#
+WWWWWWWWWWWWWWWWWWWWWWWWNXK000000KXNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWNKkdl:,'...........,;cok0NWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWKxl,.        .;.           .'cd0NWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWXx:.            oW0xl.            .;o0WWWWWWWWWWWWWWWWWW
+WWWWWWWWW0l.              .kMMMWO.              .:kNWWWWWWWWWWWWWWW
+WWWWWWW0c.                '0MMMMWd                 ;ONWWWWWWWWWWWWW
+WWWWWXd.                  .xWMMMWo                  .cKWWWWWWWWWWWW
+WWWW0;               ..    .ckKXx.      .;.           'kWWWWWWWWWWW
+WWWO'               .dO:.    .lkc    .,o0X:            .xNWWWWWWWWW
+WWO'                '0MW0l.  ,k0c .:xKWMMWc             .dWWWWWWWWW
+WK,                 .kMMMWO' lNK;.kWMMMMMK,              .kWWWWWWWW
+Nl                   'OWMMN:.OMk.oMMMMMMXc                ;KWWWWWWW
+0'                    .;ldc.lXXc lWMMWKd,                 .xWWWWWWW
+d                          ,00l, .,:;,.                    :WWWWWWW
+c                        .c0NKOkoclc.                      ,WWWWWWW
+:                     .;lcdXO,'cd;'cd:                     'WWWWWWW
+c                   .cl:. lNl   'oc..oo'                   'WWWWWWW
+o                .;lc,    oK,    .:o,.;dc.                 ;WWWWWWW
+O.              .ox.      cx.      'ol..ld,                oWWWWWWW
+N:              ;0k'      ':.       .dd. ,dl.             ,0WWWWWWW
+WO.             od;lc.             .:o;   .cd,           .dWWWWWWWW
+WWx.            :x;.:o,          ,cl;.      lk.          lNWWWWWWWW
+WWNd.            'do.'ll.    .,:lc'       .:d:          cXWWWWWWWWW
+WWWWk.            .cx:.;oc,,:cl;.       'cl:.         .oNWWWWWWWWWW
+WWWWW0:             'dd'.;:'.        .:lc,           ,kWWWWWWWWWWWW
+WWWWWWNx,             :xc.         'cl:.           .oXWWWWWWWWWWWWW
+WWWWWWWWXx,            .ox:.    .:lc'            'oKWWWWWWWWWWWWWWW
+WWWWWWWWWWNkc.           ,xxlclll:.           .:xXWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWXkl,.         ';;,.          .'cxKNWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWNKko:,..            ..,:lx0NWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWNKOxolc:::::coxOKNWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+ ▄▀▄▄▄▄   ▄▀▀▄ ▄▀▀▄  ▄▀▀▄ ▄▀▄  ▄▀▀▀▀▄  ▄▀▀▀▀▄   ▄▀▀▀█▄    ▄▀▀▀█▀▀▄ 
+ █ █    ▌ █   █    █ █  █ ▀  █ █ █   ▐ █      █ █  ▄▀  ▀▄ █    █  ▐ 
+ ▐ █      ▐  █    █  ▐  █    █    ▀▄   █      █ ▐ █▄▄▄▄   ▐   █     
+   █        █    █     █    █  ▀▄   █  ▀▄    ▄▀  █    ▐      █      
+  ▄▀▄▄▄▄▀    ▀▄▄▄▄▀  ▄▀   ▄▀    █▀▀▀     ▀▀▀▀    █         ▄▀       
+ █     ▐             █    █     ▐               █         █         
+ ▐                   ▐    ▐                     ▐         ▐         
+    Copyright © 2022-Present by Cumsoft. All rights reserved.
+            This page was last updated on 5th August 2022
+            
+                https://cumsoft.wixsite.com/cumsoft
+                   https://github.com/cumsoft
+                   https://cumsoft.gumroad.com 
+#>
+
+echo ""
+
+Write-Host "
+WWWWWWWWWWWWWWWWWWWWWWWWNXK000000KXNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWNKkdl:,'...........,;cok0NWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWKxl,.        .;.           .'cd0NWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWXx:.            oW0xl.            .;o0WWWWWWWWWWWWWWWWWW
+WWWWWWWWW0l.              .kMMMWO.              .:kNWWWWWWWWWWWWWWW
+WWWWWWW0c.                '0MMMMWd                 ;ONWWWWWWWWWWWWW
+WWWWWXd.                  .xWMMMWo                  .cKWWWWWWWWWWWW
+WWWW0;               ..    .ckKXx.      .;.           'kWWWWWWWWWWW
+WWWO'               .dO:.    .lkc    .,o0X:            .xNWWWWWWWWW
+WWO'                '0MW0l.  ,k0c .:xKWMMWc             .dWWWWWWWWW
+WK,                 .kMMMWO' lNK;.kWMMMMMK,              .kWWWWWWWW
+Nl                   'OWMMN:.OMk.oMMMMMMXc                ;KWWWWWWW
+0'                    .;ldc.lXXc lWMMWKd,                 .xWWWWWWW
+d                          ,00l, .,:;,.                    :WWWWWWW
+c                        .c0NKOkoclc.                      ,WWWWWWW
+:                     .;lcdXO,'cd;'cd:                     'WWWWWWW
+c                   .cl:. lNl   'oc..oo'                   'WWWWWWW
+o                .;lc,    oK,    .:o,.;dc.                 ;WWWWWWW
+O.              .ox.      cx.      'ol..ld,                oWWWWWWW
+N:              ;0k'      ':.       .dd. ,dl.             ,0WWWWWWW
+WO.             od;lc.             .:o;   .cd,           .dWWWWWWWW
+WWx.            :x;.:o,          ,cl;.      lk.          lNWWWWWWWW
+WWNd.            'do.'ll.    .,:lc'       .:d:          cXWWWWWWWWW
+WWWWk.            .cx:.;oc,,:cl;.       'cl:.         .oNWWWWWWWWWW
+WWWWW0:             'dd'.;:'.        .:lc,           ,kWWWWWWWWWWWW
+WWWWWWNx,             :xc.         'cl:.           .oXWWWWWWWWWWWWW
+WWWWWWWWXx,            .ox:.    .:lc'            'oKWWWWWWWWWWWWWWW
+WWWWWWWWWWNkc.           ,xxlclll:.           .:xXWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWXkl,.         ';;,.          .'cxKNWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWNKko:,..            ..,:lx0NWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWNKOxolc:::::coxOKNWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+" 
+echo ""
